226bd1ce96316f94edcce17a4c987941c4679475
elvinjames0 <huythanh022302@gmail.com>
2024-04-07 15:26:56 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("."));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://your-deployed-site.com"],
+    origin: ["http://localhost:3000", "http://hotel-management-nht.vercel.app"],
   })
 );
 
